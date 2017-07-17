29a2b2199f8df5161ca5255d3f00fb90143079ad
FedCoup <preethachinnu87@gmail.com>
2017-07-16 21:12:07 -0400

    FedCoup contract updated

--- contracts/FedCoup.sol
+++ contracts/FedCoup.sol
@@ -10,9 +10,14 @@ contract FedCoup is MintableToken {
 
     using SafeMath for uint;
 
+    /*
+    * Name of the Federation Coupon System.
+    */
     string public name = "FedCoup";
 
-    /* FedCoup currency symbol. */
+    /* 
+    * FedCoup currency symbol. 
+    */
     string public symbol = "FCC";
 
     uint public decimals = 18;
@@ -23,7 +28,9 @@ contract FedCoup is MintableToken {
      */
     uint256 FCC_value = 10 finney;  
 
-    /* constant S,B coupon (federation coupon) price as 0.01 USD. */
+    /* 
+    * constant S,B coupon (federation coupon) price as 0.01 USD. 
+    */
     uint256 constant constant_coupon_price = 10 finney;  
 
     /* residual B coupons which accumulated over the period due to B coupon distribution */
@@ -35,7 +42,11 @@ contract FedCoup is MintableToken {
     /* balance of B coupons for each address */
     mapping (address => uint) balance_B_coupons;
     
-    /* Function to get FCC price. */
+    /* 
+    * Function to get FCC price. 
+    * While FedCoup contract deployment, this function delegated to FCCPriceDefault contract.
+    * In future, this function will be delegated to new contract which would determine the FCC price from the exchange data.
+    */
     FCCPrice _fCCPriceFunction;
 
     /* event to log coupon creation */
@@ -69,38 +80,86 @@ contract FedCoup is MintableToken {
         uint currentBcoupons = balance_B_coupons[msg.sender];
         uint currentScoupons = balance_S_coupons[msg.sender];
 
-        /* ideal B coupon creation for given _FCC 
-        *  Formula: number of B coupons = (given FCC * FCC price / constant_coupon_price ) 
+        /* 
+        *  ideal B coupon creation for given _FCC 
+        *  
+        *  Formula: number of B coupons =
+        *  
+        *           (given FCC * FCC price)
+        *          --------------------------      
+        *            constant_coupon_price  
         */
         uint  idealBcoupons = _FCC.mul( _fCCPriceFunction.getFCCPrice() ).div( constant_coupon_price );
-        /* ideal S coupon creation for given _FCC 
-        *  Formula: number of S coupons = (given FCC * FCC price / constant_coupon_price ) 
-        */
-        uint  idealScoupons = _FCC.mul( _fCCPriceFunction.getFCCPrice() ).div( constant_coupon_price );;
 
-        /* assign ideal B coupons to calculatedBcoupons */
+        /* 
+        *  ideal S coupon creation for given _FCC 
+        * 
+        *  Formula: number of S coupons =
+        * 
+        *          (given FCC * FCC price)
+        *        ---------------------------   
+        *          constant_coupon_price
+        */
+        uint  idealScoupons = _FCC.mul( _fCCPriceFunction.getFCCPrice() ).div( constant_coupon_price );
+
+        /* 
+        * assign ideal B coupons to calculatedBcoupons.
+        * If B coupon distribution >= 1 or <= 2, then this ideal coupon direclty goes to the user account.
+        */
         uint calculatedBcoupons = idealBcoupons;
 
-        /* add ideal coupons with current coupons */
-        currentBcoupons = currentBcoupons.add( idealBcoupons );
-        currentScoupons = currentScoupons.add( idealScoupons );
+        /* 
+        * add ideal coupons with current coupons 
+        */
+        uint idealTotalBcoupons = currentBcoupons.add( idealBcoupons );
+        uint idealTotalScoupons = currentScoupons.add( idealScoupons );
 
-        if ( currentBcoupons > 0 ) {
-            /* bCoupons = (B/S)*_FCC */
-            uint couponDist = currentBcoupons.div(currentScoupons);
-            /* if S,B coupon distribution <= 1, then multiply with ideal B coupons */
-            if ( couponDist <= 1 ) {
-                calculatedBcoupons = couponDist.mul( idealBcoupons );
+        if ( idealTotalBcoupons > 0 ) {
+            /* 
+            * BCouponDist = (currentBcoupons + idealBcoupons)/ (currentScoupons + idealScoupons))
+            */
+            uint BCouponDist = idealTotalBcoupons.div(idealTotalScoupons);
+            /* if B coupon distribution < 1, then multiply with ideal B coupons */
+            if ( BCouponDist < 1 ) {
+                /*
+                * calculatedBcoupons = BCouponDist*idealBcoupons; 
+                */
+                calculatedBcoupons = BCouponDist.mul( idealBcoupons );
+                /* 
+                * add remaining B coupons which is part of ideal B coupons but not added to the calculated B coupons (which direclty goes to the user account) 
+                */
                 residualBcoupons = residualBcoupons.add(idealBcoupons.sub(calculatedBcoupons));
-            } else {
-                /* if S,B coupon distribution > 1, then */     
+            } else if ( BCouponDist > 2 ) {
+                /* 
+                * if B coupon distribution > 2, then following formula will be applied
+                * 
+                * Formula:  calculatedBcoupons = 
+                *    |    (currentBcoupons + idealBcoupons)                                      |
+                *    |  ------------------------------------------------------------------------ | * idealBcoupons
+                *    |    (currentBcoupons + idealBcoupons) - (currentScoupons + idealScoupons)  |         
+                */
+                BCouponDist = idealTotalBcoupons.div( idealTotalBcoupons.sub(idealTotalScoupons) );
+                calculatedBcoupons = BCouponDist.mul( idealBcoupons );
             }             
+        } else {
+            /*
+            * if ideal total B coupons is 0, then user don't have any balance coupons in the account and user passing 0 as the given FCC.
+            * otherwise throw.
+            */
+            if (idealTotalBcoupons > 0 || idealScoupons > 0 ) {
+                throw;
+            }
+            
         }
   
-        /* add created B coupons to sender */
+        /* 
+        * add created B coupons to sender 
+        */
         balance_B_coupons[msg.sender] = balance_B_coupons[msg.sender].add( calculatedBcoupons );
 
-        /* add created S coupons to sender */
+        /* 
+        * add ideal S coupons to sender 
+        */
         balance_S_coupons[msg.sender] = balance_S_coupons[msg.sender].add( idealScoupons );
 
         /* log event */
@@ -174,4 +233,9 @@ contract FedCoup is MintableToken {
     function setFCCPriceFunction(address addrFCCPriceFunc) onlyPayloadSize(2 * 32) onlyOwner {
         _fCCPriceFunction = FCCPrice(addrFCCPriceFunc);
     }
+
+    /*
+    * 
+    */
+    function 
 }
