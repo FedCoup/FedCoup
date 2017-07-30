d3eced139439387821389cfa975cf14f016cb14a
FedCoup <preethachinnu87@gmail.com>
2017-07-29 23:02:33 -0400

    FedCoup contract updated

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,6 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MetaCoin = artifacts.require("./MetaCoin.sol");
 var FedCoup = artifacts.require("./FedCoup.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
   //deploy fedcoup
   deployer.deploy(FedCoup);
 };
